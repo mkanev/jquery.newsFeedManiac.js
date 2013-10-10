8a25f55fab0672ff44d54116fb3521f62e1a6fdb
Maksim Kanev <maksim.kanev@gmail.com>
2013-10-10 18:06:59 +0400

    jsFiddle demo remove

--- demo/demo.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function ($) {
-    $('#feedHolder').activateManiac({
-        channels: [
-            {type: 'rss', id: 'http://feeds.feedburner.com/good/lbvp'}
-        ] //
-        , count: 30 //
-    });
-});
